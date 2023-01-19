e8c2237e85a3c2d28e68cf94f986720b740a2cfb
Elmin <elmin@appit.ba>
2023-01-19 14:31:28 +0100

    Fix getting id

---
+++ praksaApp/Models/Building/BuildingView.py
@@ -0,0 +1,40 @@
+from .BuildingModel import Building
+from .BuildingSerializer import BuildingSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET', 'POST'])
+def BuildingList(request):
+    if request.method == 'GET':
+        building = Building.objects.all()
+        serializer = BuildingSerializer(building, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    if request.method == 'POST':
+        serializer = BuildingSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
+def BuildingDetail(request, id):
+    
+    try:
+        building = Building.objects.get(buildingId = id)
+    except Building.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = BuildingSerializer(building)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        serializer = BuildingSerializer(building, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        building.isActive = False
+        serializer = BuildingSerializer(building)
+        serializer.save()
+        
---
+++ praksaApp/Models/Company/CompanyView.py
@@ -0,0 +1,40 @@
+from .CompanyModel import Company
+from .CompanySerializer import CompanySerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+@api_view(['GET', 'POST'])
+def CompanyList(request):
+    if request.method == 'GET':
+        company = Company.objects.all()
+        serializer = CompanySerializer(company, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    if request.method == 'POST':
+        serializer = CompanySerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+@api_view(['GET', 'PUT', 'DELETE'])
+def CompanyDetail(request, id):
+    
+    try:
+        company = Company.objects.get(companyId = id)
+    except Company.DoesNotExist:
+        return Response(status = status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
+        serializer = CompanySerializer(company)
+        return Response(serializer.data)
+    if request.method == 'PUT':
+        serializer = CompanySerializer(company, data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        company.isActive = False
+        serializer = CompanySerializer(company)
+        serializer.save()
+        
