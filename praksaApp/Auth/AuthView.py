3ac42ccd16d60176ecb40919ee6b295a424ccb4e
MirzadS <mirzad.smoloo@gmail.com>
2023-02-02 02:38:56 +0100

    Company Registration API Created and API Data Validation

--- praksaApp/Auth/AuthView.py
+++ praksaApp/Auth/AuthView.py
@@ -4,8 +4,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 import json
+from praksaApp.Models.AppartmentPerson.AppartmentPersonModel import AppartmentPerson
 from praksaApp.Models.Building.BuildingModel import Building
 from praksaApp.Models.Building.BuildingSerializer import BuildingSerializer
+from praksaApp.Models.Company.CompanyModel import Company
 
 from praksaApp.Models.Person.PersonModel import Person
 from praksaApp.Models.Person.PersonSerializer import PersonSerializer
@@ -15,65 +17,126 @@ from praksaApp.Models.RolePerson.RolePersonModel import RolePerson
 from praksaApp.Models.RolePerson.RolePersonSerializer import RolePersonSerializer
 from praksaApp.Models.RolePerson.RolePersonView import RoleGetByUser
 from praksaApp.Models.UserAccount.UserAccountModel import UserAccount
+from rest_framework import serializers
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
 
 @api_view(['POST'])
-def Login(request):      
-    try:
-        user_account = UserAccount.objects.get(username = request.data['username'])        
-    except UserAccount.DoesNotExist:
-        return Response("User does not exist", status=status.HTTP_404_NOT_FOUND)
+def Login(request):
+    login_serializer = LoginSerializer(data=request.data)
 
-    if(user_account.password == request.data['password']):
-        person = Person.objects.get(userAccountId = user_account.userAccountId)
-        person_serializer = PersonSerializer(person)
-        print(person_serializer.data) 
-
+    if login_serializer.is_valid():
         try:
-            role_person = RolePerson.objects.filter(personId = person.personId)
+            user_account = UserAccount.objects.get(username = request.data['username'])        
+        except UserAccount.DoesNotExist:
+            return Response("User does not exist", status=status.HTTP_404_NOT_FOUND)
 
-            list_of_roles = []
+        if(user_account.password == request.data['password']):
+            person = Person.objects.get(userAccountId = user_account.userAccountId)
+            person_serializer = PersonSerializer(person)
 
-            for role in role_person:
-                list_of_roles.append(role.roleId.roleName)
+            try:
+                role_person = RolePerson.objects.filter(personId = person.personId)
 
-        except RolePerson.DoesNotExist:
-            return Response("The role of a person does not exist",status = status.HTTP_404_NOT_FOUND)
+                list_of_roles = []
+
+                for role in role_person:
+                    list_of_roles.append(role.roleId.roleName)
+
+            except RolePerson.DoesNotExist:
+                return Response("Role not found.",status = status.HTTP_404_NOT_FOUND)
+
+            building_ids = []
+
+            try:
+                appartment_person_list = AppartmentPerson.objects.filter(personId = person.personId)
+
+                for app_person in appartment_person_list:
+                    building_ids.append(app_person.appartmentId.buildingId.buildingId)
+            except AppartmentPerson.DoesNotExist:
+                return Response("Building not found.",status=status.HTTP_404_NOT_FOUND)
+
+            data = {   
+                    "person":person_serializer.data, 
+                    "building_ids":building_ids,
+                    "list_of_roles":list_of_roles
+                    }
+
+            return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            return Response("Incorrect password. Please try again.", status=status.HTTP_401_UNAUTHORIZED)
+    else:
+        return Response("Invalid data received.",status=status.HTTP_400_BAD_REQUEST)
     
 
-        try:
-            building = Building.objects.get(buildingId__appartmentId__personId = person.personId)
 
-            building_serializer = BuildingSerializer(building)
-        except Building.DoesNotExist:
-            return Response("The building does not exist",status=status.HTTP_404_NOT_FOUND)
 
-        data = {   
-                "person":person_serializer.data, 
-                "building":building_serializer.data,
-                "list_of_roles":list_of_roles
-                }
+class CompanyRegistrationSerializer(serializers.Serializer):
+    firstName = serializers.CharField()
+    lastName = serializers.CharField()
+    username = serializers.CharField()
+    password = serializers.CharField()
+    dateOfBirth = serializers.DateField()
+    companyName = serializers.CharField()
+    isCompany = serializers.BooleanField()
 
-        return Response(data, status=status.HTTP_201_CREATED)
-    else:
-        return Response("The Password You Entered Is Incorrect Please Try Again", status=status.HTTP_401_UNAUTHORIZED)
+
+class RegistrationSerializer(serializers.Serializer):
+    firstName = serializers.CharField()
+    lastName = serializers.CharField()
+    username = serializers.CharField()
+    password = serializers.CharField()
+    dateOfBirth = serializers.DateField()
 
 
 @api_view(['POST'])
 def Registration(request):
-    try:
-        user_account = UserAccount.objects.get(username = request.data['username'])
-    except UserAccount.DoesNotExist:
-        user__id = UserAccount.objects.create(username=request.data['username'], password=request.data['password'])
+    registration_serializer = RegistrationSerializer(data=request.data)
 
-        person__id = Person.objects.create(firstName=request.data['firstName'], lastName=request.data['lastName'],dateOfBirth=request.data['dateOfBirth'],userAccountId = user__id)
-        
+    if registration_serializer.is_valid():
         try:
-            tenant_role = Role.objects.get(roleName = "tenant")
-            RolePerson.objects.create(personId = person__id, roleId = tenant_role)
-        except Role.DoesNotExist:
-            return Response("The role does not exist",status=status.HTTP_404_NOT_FOUND)
+            user_account = UserAccount.objects.get(username = request.data['username'])
+        except UserAccount.DoesNotExist:
+            user__id = UserAccount.objects.create(username=request.data['username'], password=request.data['password'])
+            person__id = Person.objects.create(firstName=request.data['firstName'], lastName=request.data['lastName'],dateOfBirth=request.data['dateOfBirth'],userAccountId = user__id)
+            
+            try:
+                tenant_role = Role.objects.get(roleName = "tenant")
+                RolePerson.objects.create(personId = person__id, roleId = tenant_role)
+            except Role.DoesNotExist:
+                return Response("Role not found.",status=status.HTTP_404_NOT_FOUND)
+            
+            return Response("Registration Successful.", status=status.HTTP_201_CREATED)
+        return Response("Registration failed. The username is already taken.",status=status.HTTP_409_CONFLICT)
+    else:
+        return Response("Invalid data received.",status=status.HTTP_400_BAD_REQUEST)
 
-        
-        return Response("Successful registration.", status=status.HTTP_201_CREATED)
-    
-    return Response("Unsuccessful registration. Username is already registered",status=status.HTTP_409_CONFLICT)
+
+
+@api_view(['POST'])
+def CompanyRegistration(request):
+    company_registration_serializer = CompanyRegistrationSerializer(data=request.data)
+
+    if company_registration_serializer.is_valid():
+        try:
+            user_account = UserAccount.objects.get(username = request.data['username'])
+        except UserAccount.DoesNotExist:
+            user__id = UserAccount.objects.create(username=request.data['username'], password=request.data['password'])
+            company__id = Company.objects.create(companyName=request.data['companyName'])
+            person__id = Person.objects.create(firstName=request.data['firstName'], lastName=request.data['lastName'],dateOfBirth=request.data['dateOfBirth'],companyId=company__id,userAccountId = user__id)
+            
+            try:
+                company_role = Role.objects.get(roleName = "company")
+                RolePerson.objects.create(personId = person__id, roleId = company_role)
+            except Role.DoesNotExist:
+                return Response("Role not found.",status=status.HTTP_404_NOT_FOUND)
+
+            return Response("Registration Successful.", status=status.HTTP_201_CREATED)
+        return Response("Registration failed. The username is already taken.",status=status.HTTP_409_CONFLICT)
+    else:
+        return Response("Invalid data received.",status=status.HTTP_400_BAD_REQUEST)
+
